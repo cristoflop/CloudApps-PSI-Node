a5f623df8f9b9a22bd93c3c68334d13b2d5f6e46
cristoflop <cristofer.lc.99@gmail.com>
2021-01-26 00:28:34 +0100

    Added test case saving film with mocked db

--- test/mockBd/api.test.js
+++ test/mockBd/api.test.js
@@ -1,17 +1,11 @@
 "use strict"
 
-const app = require('../src/app');
+const app = require('../../src/app');
 const AWS = require("aws-sdk-mock");
 
 const supertest = require('supertest');
 
 const request = supertest(app);
-
-const newMockedFilm = {
-    id: 1,
-    title: "La guerra de las galaxias",
-    author: "George Lucas"
-}
 
 const mockedFilms = [
     {
@@ -42,5 +36,17 @@ test('Get films endpoint', async () => {
         .expect(200);
 
     expect(response.body.length).toBe(3);
+
+});
+
+test('Save films endpoint', async () => {
+
+    AWS.mock('DynamoDB.DocumentClient', 'put', function (params, callback) {
+        callback(null, "Film saved");
+    });
+
+    const response = await request.post('/api/films/')
+        .expect('Content-Type', /json/)
+        .expect(201);
 
 });
